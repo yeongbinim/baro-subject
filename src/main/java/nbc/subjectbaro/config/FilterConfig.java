6abfa6e73458fda4d0e89b464d081bef07a5f1ff
yeongbinim <yeim.dev@gmail.com>
2025-04-18 15:41:12 +0900

    chore: Filter 추가 및 PasswordEncoder 추가

---
+++ src/main/java/nbc/subjectbaro/config/FilterConfig.java
@@ -0,0 +1,23 @@
+package nbc.subjectbaro.config;
+
+import lombok.RequiredArgsConstructor;
+import nbc.subjectbaro.domain.common.util.JwtUtil;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class FilterConfig {
+
+    private final JwtUtil jwtUtil;
+
+    @Bean
+    public FilterRegistrationBean<JwtFilter> jwtFilter() {
+        FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new JwtFilter(jwtUtil));
+        registrationBean.addUrlPatterns("/*");
+
+        return registrationBean;
+    }
+}
