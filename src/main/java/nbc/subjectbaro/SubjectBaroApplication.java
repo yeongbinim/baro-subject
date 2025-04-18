00a2ae59a4fbc76e765061f4ebe02d602a3b49d6
yeongbinim <yeim.dev@gmail.com>
2025-04-18 14:35:55 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/nbc/subjectbaro/SubjectBaroApplication.java
@@ -0,0 +1,13 @@
+package nbc.subjectbaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectBaroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubjectBaroApplication.class, args);
+    }
+
+}
